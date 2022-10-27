8b18e888b9d29f0e9973bbb48c75bc8a0ca57cff
ANKIT RAUTELA <ankitrautela044@gmail.com>
2022-10-27 08:08:24 +0530

    finish with Home page and about page

--- src/About.js
+++ src/About.js
@@ -1,7 +1,11 @@
 import React from "react";
+import HeroSection from "./components/HeroSection";
 
 const About = () => {
-  return <div>About</div>;
+  const data = {
+    name: "Farmer's Ecommerce",
+  };
+  return <HeroSection myData={data} />;
 };
 
 export default About;
