35e42b83d5de39efc7ab6424b1d6006eb5d005da
ANKIT RAUTELA <ankitrautela044@gmail.com>
2022-11-01 07:55:16 +0530

    Footer Finish

--- src/Contact.js
+++ src/Contact.js
@@ -33,7 +33,55 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact Page</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224567.6618301994!2d76.84966220020254!3d28.423187823880255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d19d582e38859%3A0x2cf5fe8e5c64b1e!2sGurugram%2C%20Haryana!5e0!3m2!1sen!2sin!4v1667236527419!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/mlevpyqn"
+            method="POST"
+            className="contact-inputs"
+          >
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+              // value=""
+            />
+            <input
+              type="Email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+              // value=""
+            />
+            <textarea
+              name="Message"
+              placeholder="Enter your message"
+              cols="30"
+              rows="10"
+              autoComplete="off"
+            ></textarea>
+
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
